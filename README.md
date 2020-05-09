9a36853b10725619a69e1338cdc63edf5ee59b23
Michael Northcutt <michael_northcutt@yahoo.com>
2020-05-08 20:00:11 -0500

    adding project template and user registration/authentication setup

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Snapped
+
+## Overview
+
+
+Check out the app on Heroku [here]()!
+
+## Installation Instructions
+
+
+## How to Use
+
+
+## Technologies Used
+- Node
+- Express
+- Handlebars
+- MongoDB
+- Mongoose
+- Cheerio
+- Axios
--- server.js
+++ server.js
@@ -1,24 +1,12 @@
+/* eslint-disable no-console */
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const { join } = require("path");
+const morgan = require("morgan");
 const app = express();
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+const port = process.env.SERVER_PORT || 3000;
 
-// Define API routes here
+app.use(morgan("dev"));
+app.use(express.static(join(__dirname, "build")));
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
