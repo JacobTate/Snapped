ddad3db74f8fa403cd33d494e160d08fd3550f03
Jessie Taylor <jtaylor@jumpcrew.com>
2020-05-12 18:49:05 -0500

    added the first set of photos to the home page and added images to the file structure

---
+++ client/src/utils/ImageData.js
@@ -0,0 +1,76 @@
+const contentData = [
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  },
+  {
+    tag: "beach girl",
+    link: "/assets/pic1.png",
+    description:
+      "Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server."
+  }
+];
+
+export default contentData;
