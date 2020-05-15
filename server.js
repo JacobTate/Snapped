238a3d225ebb76a4be6d3e62ddc2779c287746a5
Sauda281 <58537112+Sauda281@users.noreply.github.com>
2020-05-15 15:15:37 -0500

    adding database URI, and .env file

--- server.js
+++ server.js
@@ -2,11 +2,46 @@
 const express = require("express");
 const { join } = require("path");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const methodOverride = require('method-override');
+
 const app = express();
 
 const port = process.env.SERVER_PORT || 3001;
 
-app.use(morgan("dev"));
+app.use(express.json());
 app.use(express.static(join(__dirname, "build")));
+app.use(methodOverride('_method'));
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect('uri', {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () =>{   
+    console.log("Mongodb database connection successful");
+})
+
+
+// app.use(morgan("dev"));
+// app.use(express.static(join(__dirname, "build")));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const { join } = require("path");
+// const morgan = require("morgan");
+// const app = express();
+
+// const port = process.env.SERVER_PORT || 3001;
+
+// app.use(morgan("dev"));
+// app.use(express.static(join(__dirname, "build")));
+
+// app.listen(port, () => console.log(`Listening on port ${port}`));
