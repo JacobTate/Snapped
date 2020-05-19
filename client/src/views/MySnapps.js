187f8ab6e8108e1d7569399a35e217786274034c
Jessie Taylor <jtaylor@jumpcrew.com>
2020-05-19 18:27:20 -0500

    added file name to upload bar in mysnapps

--- client/src/views/MySnapps.js
+++ client/src/views/MySnapps.js
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "reactstrap";
- import axios from "axios";
+import axios from "axios";
 // import {
 //   Card, Button, CardImg, CardTitle, CardText, CardDeck,
 //   CardSubtitle, CardBody
@@ -10,28 +10,39 @@ import { useAuth0 } from "../react-auth0-spa";
 import ImageRender from "../utils/ImageRender";
 
 //class MySnapps extends Component {
-  const MySnapps = function () {
+const MySnapps = function () {
   const { loading, user } = useAuth0();
-  
-    return (
-      <Container>
-            <form action="/upload" method="POST" encType="multipart/form-data">
-            <p className="lead text-muted">{user.email}</p>
-            <input type="hidden" name="userEmail" value={user.email} 
-                        useRef={user.email} />
-              <div className="custom-file mb-3">
-                <input accept="image/x-png,image/gif,image/jpeg" type="file" name="file" id="file" className="custom-file-input"></input>
-                <label htmlFor="file" className="custom-file-label">Choose File</label>
-              </div>
-              <input type="submit" value="Submit" className="btn btn-primary btn-block"></input>
-            </form>
-        
-        <ImageRender userEmail={user.email}/>    
-      
-      </Container>
-      
-    );
+
+  //fileselected is a custom global variable 
+  //setFileSelected is a custom functon to update the fileselected global variable
+  //useState allows you to have a custom state object for data storage
+  const [fileSelected, setFileSelected ] = useState("")
+
+  const handleChange = event => {
+    //fileSelected="iamge.png"
+    //setFileSelected("image.png")
+    console.log(`Selected file - ${event.target.files[0].name}`);
+    setFileSelected(event.target.files[0].name)
   }
+  return (
+    <Container>
+      <form action="/upload" method="POST" encType="multipart/form-data">
+        <p className="lead text-muted">{user.email}</p>
+        <input type="hidden" name="userEmail" value={user.email}
+          useRef={user.email} />
+        <div className="custom-file mb-3">
+          <input accept="image/x-png,image/gif,image/jpeg " type="file" name="file" id="file" className="custom-file-input" onChange={handleChange}></input>
+          <label for="file" className="custom-file-label">{fileSelected.length >0? fileSelected : "Choose File"}</label>
+        </div>
+        <input type="submit" value="Submit" className="btn btn-primary btn-block"></input>
+      </form>
+
+      <ImageRender userEmail={user.email} />
+
+    </Container>
+
+  );
+}
 
 
 export default MySnapps;
