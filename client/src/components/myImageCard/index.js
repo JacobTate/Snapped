96592b2ae663cd70525dbebf273070d4b0458326
Jacob Tate <jacob87608@gmail.com>
2020-05-20 21:12:23 -0500

    added modal and form to the mysnapps page when the tag button is clicked image objectId's are added to the selected tag's 'image' array

--- client/src/components/myImageCard/index.js
+++ client/src/components/myImageCard/index.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Card, Button, CardImg, CardTitle, CardText, CardDeck,
-    CardSubtitle, CardBody
+    CardSubtitle, CardBody, Modal, ModalHeader, ModalBody, ModalFooter
 } from 'reactstrap';
-
+import axios from "axios";
+import ModalForm from "../../utils/ModalForm";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // styles
 import "./myImageCard.css";
+import TagSelect from "../../utils/AactivityTagsSelect";
 
 const Example = (props) => {
+    const {
+        buttonLabel,
+        className
+      } = props;
+    
+      const [modal, setModal] = useState(false);
+    
+      const toggle = () => {
+          setModal(!modal);
+    };
     return (
+        <div>  
+        <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+            <ModalForm imageId={props.thisImage}/>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
         <CardDeck>
             <Card className="CardListItem-main-card">
                 <CardImg top width="100%" src={props.image} alt="Card image cap" className="cardSize" />
                 <CardBody>
                     <Button color="white" className="m-0"><FontAwesomeIcon icon="file-download" color="rgba(91, 192, 222, 0.75)" className="m-0" /></Button>
                     <Button color="white"className="m-0"><FontAwesomeIcon icon="trash-alt" color="rgba(91, 192, 222, 0.75)" className="m-0" /></Button>
-                    <Button color="white"className="m-0"><FontAwesomeIcon icon="tag" color="rgba(91, 192, 222, 0.75)" className="m-0" /></Button>
+                    <Button color="white" value={props.thisImage}  onClick={toggle} className="m-0 "><FontAwesomeIcon icon="tag" color="rgba(91, 192, 222, 0.75)" className="m-0" /></Button>
                 </CardBody>
             </Card>
         </CardDeck>
+        </div>
     );
 };
 
---
+++ models/activityTags.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var TagsSchema = new Schema({
+tag: {
+  type: String,
+  required: true,
+  unique: true
+},
+images: {
+  type: Array
+}
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var ActivityTags = mongoose.model("activityTags", TagsSchema);
+
+// Export the Article model
+module.exports = ActivityTags;
