5560b3969f4de9c7c4f56b48e6b6d2d0deb79769
Lynde Capogreco <lyndecapc@gmail.com>
2020-05-23 12:08:12 -0500

    picture carousel

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <Carousel autoPlay>
+    <div>
+      <img alt="" src="../assets/Snapped_Logo.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/Snapped_Logo.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/Snapped_Logo.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/Snapped_Logo.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic12.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+    <div>
+      <img alt="" src="../assets/pic1.png" />
+      <p className="legend"></p>
+    </div>
+  </Carousel>
+);
+
