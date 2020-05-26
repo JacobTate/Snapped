7e08f8c69033bac0dae6abcc13a16eaa94be1957
Jessie Taylor <jtaylor@jumpcrew.com>
2020-05-26 18:21:31 -0500

    replaced carousel, connected css file to js file, and added json file for database of images to go in map function

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,72 +1,104 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
+import "./Carousel.css";
+import  Images from "../utils/Images.json";
 
 
 export default () => (
-  
-  <Carousel autoPlay height="200px" width="200px">
-    <div>
-      <img alt="" src="../assets/Snapped_Logo.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/Snapped_Logo.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/Snapped_Logo.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/Snapped_Logo.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic12.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-    <div>
-      <img alt="" src="../assets/pic1.png" />
-      <p className="legend"></p>
-    </div>
-   
+  <div className="slider-container">
+
+    <Carousel autoPlay>
+{
+  Images.map((image,index) => {
+    return(
+<div key = {index}>
+        <img alt={image.alt} src={image.path} />
+    <p className="legend">Legend {index + 1}</p>
+    
+        <img alt={image.alt} src={image.path}/>
+        <p className="legend">Legend {index + 1}</p>
+      </div>
+    )
+  })
+}
+
+      
+
+
     </Carousel>
+  </div>
+
+
+
+
+  // <Carousel autoPlay height="200px" width="200px">
+  //   <div>
+  //     <img alt="" src="../assets/Snapped_Logo.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/Snapped_Logo.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/Snapped_Logo.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/Snapped_Logo.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic12.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+  //   <div>
+  //     <img alt="" src="../assets/pic1.png" />
+  //     <p className="legend"></p>
+  //   </div>
+
+  // </Carousel>
+
+
+
+
+
 );
 
 
- 
+
 //     <Carousel autoPlay height="200px" width="200px">
 // <div>
 //       <img alt="" src="../assets/Snapped_Logo.png" />
