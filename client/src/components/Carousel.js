fc8dc047bff5f97b8c59ffd7420d3b2239d5000a
Jessie Taylor <jtaylor@jumpcrew.com>
2020-05-27 21:58:54 -0500

    completely took out carousel and replaced with multi carousel

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,161 +1,54 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import Images from "../utils/Images.json";
+import "semantic-ui-css/semantic.min.css";
+import { Image } from "semantic-ui-react";
 import "./Carousel.css";
-import  Images from "../utils/Images.json";
 
 
-export default () => (
-  <div className="slider-container">
+export default () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
+  return (
+    <div>
+      <Carousel
+        ssr
+        partialVisbile
+        // deviceType={deviceType}
+        itemClass="image-item"
+        responsive={responsive}
+      >
+        {
+          Images.map((image, index) => {
+            return (
+              <Image src={image.path} style={{ width: "100%", height: "100%" }} />
 
-    <Carousel autoPlay>
-{
-  Images.map((image,index) => {
-    return(
-<div key = {index}>
-        <img alt={image.alt} src={image.path} />
-    <p className="legend">Legend {index + 1}</p>
-    
-        <img alt={image.alt} src={image.path}/>
-        <p className="legend">Legend {index + 1}</p>
-      </div>
-    )
-  })
+
+            )
+          })
+        }
+      </Carousel>
+    </div>
+  );
 }
 
-      
-
-
-    </Carousel>
-  </div>
 
 
 
-
-  // <Carousel autoPlay height="200px" width="200px">
-  //   <div>
-  //     <img alt="" src="../assets/Snapped_Logo.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/Snapped_Logo.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/Snapped_Logo.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/Snapped_Logo.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic12.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-  //   <div>
-  //     <img alt="" src="../assets/pic1.png" />
-  //     <p className="legend"></p>
-  //   </div>
-
-  // </Carousel>
-
-
-
-
-
-);
-
-
-
-//     <Carousel autoPlay height="200px" width="200px">
-// <div>
-//       <img alt="" src="../assets/Snapped_Logo.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/Snapped_Logo.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/Snapped_Logo.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/Snapped_Logo.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic12.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//     <div>
-//       <img alt="" src="../assets/pic1.png" />
-//       <p className="legend"></p>
-//     </div>
-//   </Carousel>
-// );
 
