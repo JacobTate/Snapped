37d26be917fc29857db750f9033a67b61f5884b8
Jacob Tate <jacob87608@gmail.com>
2020-05-31 13:20:44 -0500

    added mongo uri and auth0 credentials to .env changes to server for deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 require("dotenv").config();
 
-const port = process.env.SERVER_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: true}));
@@ -16,24 +16,10 @@ app.use(express.static("public"));
 app.use(methodOveride("_method"));
 app.use(express.static(join(__dirname, "build")));
 require("./controllers/controller")(app);
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const { join } = require("path");
-// const morgan = require("morgan");
-// const app = express();
-
-// const port = process.env.SERVER_PORT || 3001;
-
-// app.use(morgan("dev"));
-// app.use(express.static(join(__dirname, "build")));
-
-// app.listen(port, () => console.log(`Listening on port ${port}`));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  };
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
