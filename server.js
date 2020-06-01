0c5cf14b49a89ecb7813030ce91971a7b48ca084
JacobTate <57567558+JacobTate@users.noreply.github.com>
2020-06-01 18:21:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   };
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
